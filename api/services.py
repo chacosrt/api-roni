198cd44f9491071ab4a1832894f08fd9be8f5a37
Jesus Antonio Hernandez Rangel <1203098@onuriscp.com>
2026-03-10 08:52:56 -0600

    *

--- api/services.py
+++ api/services.py
@@ -724,7 +724,7 @@ def get_partidos_por_jornada(
         db.query(_models.Partidos)        
         .filter(_models.Partidos.liga.in_([1,2]))          
         .filter(_models.Partidos.temporada == temporada)
-        #.filter(_models.Partidos.jornada == jornada)
+        .filter(func.max(_models.Partidos.jornada))
         .order_by(_models.Partidos.liga.asc())   
         .all()
     )
